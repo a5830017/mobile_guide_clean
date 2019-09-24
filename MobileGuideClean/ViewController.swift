7cc56d2c0dc7553d8c8945d350c76c57425ac6b6
Jiratip <a5830017a@gmail.com>
2019-09-24 15:10:58 +0700

    start clean

---
+++ MobileGuideClean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileGuideClean
+//
+//  Created by Jiratip Hemwutthipan on 24/9/2562 BE.
+//  Copyright © 2562 Jiratip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
