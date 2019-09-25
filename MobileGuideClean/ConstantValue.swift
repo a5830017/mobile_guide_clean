4e7b7ae091402b958afcd58ed3ed315955ff3c12
Jiratip <a5830017a@gmail.com>
2019-09-25 15:07:02 +0700

    add constant value

---
+++ MobileGuideClean/ConstantValue.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantValue.swift
+//  MobileGuideClean
+//
+//  Created by Jiratip Hemwutthipan on 24/9/2562 BE.
+//  Copyright © 2562 Jiratip. All rights reserved.
+//
+
+import Foundation
+
+enum SortType {
+    case priceLowToHigh
+    case priceHighToLow
+    case rating
+}
+
+enum SegmentState {
+    case all
+    case favourite
+}
