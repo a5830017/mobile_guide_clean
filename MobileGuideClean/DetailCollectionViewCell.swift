df138915a52240b4fbb73f1e1259d9a43b1713bd
Jiratip <a5830017a@gmail.com>
2019-09-26 09:56:22 +0700

    add detail scene

---
+++ MobileGuideClean/DetailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DetailCollectionViewCell.swift
+//  MobileGuideClean
+//
+//  Created by Jiratip Hemwutthipan on 26/9/2562 BE.
+//  Copyright © 2562 Jiratip. All rights reserved.
+//
+
+import UIKit
+
+class DetailCollectionViewCell: UICollectionViewCell {
+    
+}
