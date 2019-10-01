95cac8fd2fa358124039a4bda2d6f0c6f8c73ff6
Jiratip <a5830017a@gmail.com>
2019-10-01 16:17:31 +0700

    unit test : test on favourite list and sort rating

--- MobileGuideCleanTests/Scenes/MobileListScene/MobileListInteractorTests.swift
+++ MobileGuideCleanTests/Scenes/MobileListScene/MobileListInteractorTests.swift
@@ -336,22 +336,22 @@ class MobileListInteractorTests: XCTestCase {
         XCTAssertTrue(mobileListPresenterSpy.presentFeatureCalled)
 
     }
-//
-//    func testFavouriteAndSortRating() {
-//        //Given
-//        let mobileListPresenterSpy = MobileListPresenterSpy()
-//        sut.presenter = mobileListPresenterSpy
-//        sut.mobileList = MobileListMock.Mobile.mobileList
-//        sut.favList = MobileListMock.Mobile.favList
-//
-//        //When
-//        let request : MobileList.FeatureMobile.Request = MobileList.FeatureMobile.Request(segmentState: .favourite, sortType: .rating)
-//        sut.check(request: request)
-//
-//        //Then
-//        XCTAssertTrue(mobileListPresenterSpy.presentFeatureCalled)
-//
-//    }
+
+    func testFavouriteAndSortRating() {
+        //Given
+        let mobileListPresenterSpy = MobileListPresenterSpy()
+        sut.presenter = mobileListPresenterSpy
+        sut.mobileList = MobileListMock.Mobile.mobileList
+        sut.favList = MobileListMock.Mobile.favList
+
+        //When
+        let request : MobileList.FeatureMobile.Request = MobileList.FeatureMobile.Request(segmentState: .favourite, sortType: .rating)
+        sut.check(request: request)
+
+        //Then
+        XCTAssertTrue(mobileListPresenterSpy.presentFeatureCalled)
+
+    }
     
     
 }
