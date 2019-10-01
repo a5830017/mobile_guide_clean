9f66a59afef6fa54c9577d59e70361507433aeda
Jiratip <a5830017a@gmail.com>
2019-10-01 15:59:39 +0700

    remove init for unit test

--- MobileGuideClean/Scenes/MobileListScene/MobileListEntity.swift
+++ MobileGuideClean/Scenes/MobileListScene/MobileListEntity.swift
@@ -17,16 +17,4 @@ struct MobileModel : Codable {
     let rating : Float
     let id : Int
     var isFavourite : Bool?
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.thumbImageURL = try container.decodeIfPresent(String.self, forKey: .thumbImageURL)!
-        self.brand = try container.decodeIfPresent(String.self, forKey: .brand)!
-        self.price = try container.decodeIfPresent(Float.self, forKey: .price)!
-        self.description = try container.decodeIfPresent(String.self, forKey: .description)!
-        self.name = try container.decodeIfPresent(String.self, forKey: .name)!
-        self.rating = try container.decodeIfPresent(Float.self, forKey: .rating)!
-        self.id = try container.decodeIfPresent(Int.self, forKey: .id)!
-        self.isFavourite = try container.decodeIfPresent(Bool.self, forKey: .isFavourite) ?? false
-    }
 }
